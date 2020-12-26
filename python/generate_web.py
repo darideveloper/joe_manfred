bebce9bf1799fe53b8774b01e9c4baf2c532ab8d
dari <hernandezdarifrancisco@gmail.com>
2020-12-25 22:36:46 -0600

    Boards templates

--- python/generate_web.py
+++ python/generate_web.py
@@ -13,9 +13,18 @@ class Generate ():
         """
 
         self.path = path_web
-        self.index_file  = open(os.path.join (self.path, "index-template.html"), "r")
-        self.to_file = open(os.path.join (self.path, "index.html"), "w")
 
+        # Index files
+        self.index_file_template  = open(os.path.join (self.path, "index-template.html"), "r")
+        self.index_file = open(os.path.join (self.path, "index.html"), "w")
+
+        # Index files
+        self.board_file_template  = open(os.path.join (self.path, "board-template.html"), "r")
+        self.board_best = open(os.path.join (self.path, "board-best.html"), "w")
+        self.board_all = open(os.path.join (self.path, "board-all.html"), "w")
+        self.board_videos = open(os.path.join (self.path, "board-videos.html"), "w")
+
+        # Data CSV
         csv_file = open (os.path.join (os.path.dirname (__file__), "data.csv"))
         self.data = self.__get_data (csv_file)
 
@@ -49,11 +58,15 @@ class Generate ():
         counter_line = 0
         elements_counter = 0
 
-        for line in self.index_file.readlines():
+        # Read amns save each line of html file
+        for line in self.index_file_template.readlines():
             
             lines_html.append (line)
+
+            # Identify speicic line in the text
             elemnt = '<img src="imgs/arrow.png" alt="">'
 
+            # Get position of each line for each section
             if line.strip() == elemnt: 
                 elements_counter += 1
 
@@ -78,24 +91,24 @@ class Generate ():
         # Header
         html_text += (lines_html [:best_position]) 
         # Section Best 
-        html_text += (self.__get_index_articles_section(index_section_best))
+        html_text += (self.__get_articles_section(index_section_best, max_10 = True))
         # Best - All intersection
         html_text += (lines_html [best_position:all_position])
         # Section All
-        html_text += (self.__get_index_articles_section(index_section_all))
+        html_text += (self.__get_articles_section(index_section_all, max_10 = True))
         # Alll - Videos intersection
         html_text += (lines_html [all_position:videos_position])
         # Section videos
-        html_text += (self.__get_index_articles_section(index_section_videos))
+        html_text += (self.__get_articles_section(index_section_videos, max_10 = True))
         # footer
         html_text += (lines_html [videos_position:])
 
-
+        # Write information in file
         for line in html_text: 
-            self.to_file.write ("\n" + line.rstrip())
+            self.index_file.write ("\n" + line.rstrip())
 
 
-    def __get_index_articles_section (self, section): 
+    def __get_articles_section (self, section, max_10 = False): 
         """
         Return a list with html text lines, fromated and with information of each article of the section
         """
@@ -106,7 +119,12 @@ class Generate ():
         max_articles = len(self.data) - 1
         article_counter = 0
         articles_section = 0
-        while articles_section < 10 and article_counter <= max_articles:
+        while article_counter <= max_articles:
+
+            # Stop program if it request only ten articles
+            if max_10 and articles_section >= 10:
+                break
+
                 
             if self.data[article_counter][section].strip().lower() == 'true':
                 data_article = self.data[article_counter]
@@ -130,7 +148,104 @@ class Generate ():
 
         return articles_html
                 
-                
+    def board (self, board):
+        """
+        Generate a board html file, with the correct format and information
+        """
+
+        # to_file = os.path.basename (board)
+        # print ("Generating {} file...".format (to_file))
+
+        # all lines of the template html file
+        lines_html = []
+
+        # Line of the board in the html file
+        position_board = 0
+
+        counter_line = 0
+        for line in self.board_file_template.readlines():
+
+            lines_html.append (line)
+
+            # Identify speicic line in the text
+            elemnt = '<main>'
+
+            if line.strip() == elemnt: 
+                position_board = counter_line + 1
+            
+            counter_line+=1
+
+        # Position of each section value inside the data file
+        board_best = 6
+        board_all = 5
+        board_videos = 4
+
+
+        # Get all text and format sections
+        html_text = []
+
+        # Header
+        html_text += (lines_html [:position_board]) 
+
+        # Board
+        html_text += (self.__get_board (board_best))
+
+        # Footer
+        html_text += (lines_html [position_board:]) 
+
+        # Write information in file
+        for line in html_text: 
+            self.board_best.write ("\n" + line.rstrip())
+
+    def __get_board (self, board): 
+        """
+        Reurn a list with html text lines, formated and with image of the boards
+        """
+
+        articles_html = []
+
+        # get articles of the specificv section
+        artiles_section =  self.__get_articles_section (board, max_10 = False)
+
+        # Calculate the number of articles in each column
+        articles_num = len (artiles_section) / 8
+        articles_in_column =  round (articles_num/4) * 8
+
+        # List of list of articles in each column
+        articles_columns = []
+
+        # Generate each column with subarticles
+        articles_columns.append(artiles_section[:articles_in_column])
+        articles_columns.append(artiles_section[articles_in_column:articles_in_column*2])
+        articles_columns.append(artiles_section[articles_in_column*2:articles_in_column*3])
+        articles_columns.append(artiles_section[articles_in_column*3:])
 
         
-    
+
+        # Add title and grid open
+        articles_html.append ('        <h1>My Best Draws</h1>')
+        articles_html.append ('        <div class="board">')
+        
+
+        # Add open and close tag to each section
+        for articles_column in articles_columns:
+
+            # Add open tag of suboard
+            articles_html.append ('            <section class="sub-board">')
+
+            # Save articles of the column
+            for article in articles_column: 
+                # print (article)
+                articles_html.append (article)
+
+            # Add closed tag of suboard
+            articles_html.append ('            </section>')
+
+        # Add gir close
+
+        articles_html.append ('        </div>')
+
+        return articles_html
+
+
+        
--- python/main.py
+++ python/main.py
@@ -10,11 +10,11 @@ from extract_frames import Extract_frames
 parent_path = os.path.dirname (os.path.dirname (__file__))
 path = os.path.join (parent_path, "web")
 
-# Resize images
-origin_resize = os.path.join (path, "imgs", "all")
-destination_resize = os.path.join (path, "imgs", "small")
+# # Resize images
+# origin_resize = os.path.join (path, "imgs", "all")
+# destination_resize = os.path.join (path, "imgs", "small")
 
-myResize = Resize(origin_resize, destination_resize, auto_replace=False)
+# myResize = Resize(origin_resize, destination_resize, auto_replace=False)
 
 # # Generate frames from videos
 # videos_path = os.path.join (path, "videos")
@@ -23,5 +23,6 @@ myResize = Resize(origin_resize, destination_resize, auto_replace=False)
 # Generate Html Files
 my_generate = Generate (path)
 my_generate.index()
+my_generate.board(6)
 
 print ("Done")
